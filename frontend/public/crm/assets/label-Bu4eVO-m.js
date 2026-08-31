277e7635068dfcdc1c0a3c5570b3e2e29cc992e0
Álvaro Careli <alvaro.sampaio@gec.inatel.br>
2026-08-30 23:16:38 -0300

    fix: rebuild embedded CRM without broken vendor chunks

--- frontend/public/crm/assets/label-Bu4eVO-m.js
+++
@@ -1,1 +0,0 @@
-import{r,j as t,bl as a}from"./vendor-react-Dx8-lm_2.js";import{aI as m}from"./vendor-B48eQk9f.js";import{c as i}from"./index-BZgtvZDs.js";const l=m("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),p=r.forwardRef(({className:s,...e},o)=>t.jsx(a,{ref:o,className:i(l(),s),...e}));p.displayName=a.displayName;export{p as L};
