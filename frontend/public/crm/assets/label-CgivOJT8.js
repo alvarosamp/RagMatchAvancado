601dc50d73908d1bfa40b088f365e473819e0c62
Álvaro Careli <alvaro.sampaio@gec.inatel.br>
2026-09-03 16:00:51 -0300

    fix: ship complete portal with working CRM runtime

---
+++ frontend/public/crm/assets/label-CgivOJT8.js
@@ -0,0 +1,11 @@
+import{i as r,r as l,j as o,y as n,b as c,ay as y}from"./index-CZR9wsuy.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const b=r("Plus",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=r("Trash2",[["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6",key:"4alrt4"}],["path",{d:"M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2",key:"v07s0e"}],["line",{x1:"10",x2:"10",y1:"11",y2:"17",key:"1uufr5"}],["line",{x1:"14",x2:"14",y1:"11",y2:"17",key:"xtxkd"}]]);var u="Label",d=l.forwardRef((e,t)=>o.jsx(n.label,{...e,ref:t,onMouseDown:a=>{var s;a.target.closest("button, input, select, textarea")||((s=e.onMouseDown)==null||s.call(e,a),!a.defaultPrevented&&a.detail>1&&a.preventDefault())}}));d.displayName=u;var i=d;const x=y("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),f=l.forwardRef(({className:e,...t},a)=>o.jsx(i,{ref:a,className:c(x(),e),...t}));f.displayName=i.displayName;export{f as L,b as P,k as T};
